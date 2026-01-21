a136f705d3134c838fec115eed84a261f2553a66
Hardik-KRMU <hardikchandra713@gmail.com>
2026-01-21 15:14:21 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// const para=document.getElementsByClassName('.para')
+// console.log(para)
+
+// const para2=document.getElementsByClassName('.para')
+// console.log(para2)
+
+// const para3=document.getElementsByClassName('.para')
+// console.log(para3)
+
+// let arr=[23,45,235,56,45,3,2]
+// arr.forEach((ele)=>console.log(ele))
+
+
+// const containeer=document.querySelector(".container")
+// containeer.innerHTML="<h1>This is a heading</h1>"
+// console.log(container)
+
+// const btn=document.queryselector('button')
+// btn.classlist.add("btn")
+// // btn.classlist.remove("btn")
+// // btn.classlist.toggle("btn")
+// const click=document.queryselector('#click')
+// const stop=document.queryselector('#stop')
+
+// function message(){
+//     console.log("You clicked the button")
+// }
+
+// click.addEventListener('click',message)
+// stop.addEventListener('click',()=>{
+//     console.log("You clicked the stop button")
+//     click.removeEventListener('click',message)
+// })
+
+// const form=document.querySelector('form')
+// form.addEventListener('submit',(e)=>{
+//     event.preventDefault()
+//     console.log("Form Submitted")
+// })
+
